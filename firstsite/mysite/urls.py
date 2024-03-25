d59b54b661cb0538bc33671c89990c8081c2d3bf
AndIx11 <andrew.animod@mail.ru>
2024-03-25 22:12:38 +0700

    Выполненная вторая лабораторная по WEB

---
+++ firstsite/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, register_converter
+from . import views, converters
+
+register_converter(converters.YearMonth, 'ye4mo2da2')
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='addpage'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('category/<int:cat_id>/', views.show_category, name='cats')
+]
