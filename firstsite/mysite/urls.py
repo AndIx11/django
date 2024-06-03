6a4738868692184054dd1935007c419e55af87ce
AndIx11 <andrew.animod@mail.ru>
2024-06-03 07:51:31 +0700

    Выполненная лабораторная работа №7 по WEB`у

--- firstsite/mysite/urls.py
+++ firstsite/mysite/urls.py
@@ -4,12 +4,14 @@ from . import views, converters
 register_converter(converters.YearMonth, 'ye4mo2da2')
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.MySiteHome.as_view(), name='home'),
     path('about/', views.about, name='about'),
-    path('addpage/', views.addpage, name='addpage'),
+    path('addpage/', views.AddPage.as_view(), name='addpage'),
+    path('edit/<slug:slug>/', views.UpdatePage.as_view(), name='edit_page'),
+    path('delete/<slug:slug>/', views.DeletePage.as_view(), name='delete_page'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
-    path('post/<slug:post_slug>/', views.show_post, name='post'),
-    path('category/<slug:cat_slug>/', views.show_category, name='category'),
-    path('tag/<slug:tag_slug>/', views.show_tag_postlist, name='tag'),
+    path('post/<slug:post_slug>/', views.ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', views.MySiteCategory.as_view(), name='category'),
+    path('tag/<slug:tag_slug>/', views.TagPostList.as_view(), name='tag'),
 ]
--- firstsite/mysite/views.py
+++ firstsite/mysite/views.py
@@ -1,9 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
 from mysite.forms import AddPostForm, UploadFileForm
 from mysite.models import MySite, Category, TagPost, UploadFiles
 import uuid
+
+from mysite.utils import DataMixin
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'addpage'},
@@ -13,49 +20,109 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 
 
 # Create your views here.
-def show_tag_postlist(request, tag_slug):
-    tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=MySite.Status.PUBLISHED)
-    data = {
-        'title': f'Тег: {tag.tags}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': None,
-    }
-    return render(request, 'mysite/index.html', context=data)
+class TagPostList(DataMixin, ListView):
+    template_name = 'mysite/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag = TagPost.objects.get(slug=self.kwargs['tag_slug'])
+        return self.get_mixin_context(context, title='Тег: ' + tag.tag)
+
+    def get_queryset(self):
+        return (MySite.published.filter(tags__slug=self.kwargs['tag_slug'])
+                .select_related('cat'))
 
 
-def show_category(request, cat_slug):
-    category = get_object_or_404(Category, slug=cat_slug)
-    posts = MySite.published.filter(cat_id=category.pk)
-    data = {
-        'title': f'Рубрика: {category.name}',
-        'menu': menu,
-        'posts': posts,
-        'cat_selected': category.pk,
-    }
-    return render(request, 'mysite/index.html', context=data)
+#def show_tag_postlist(request, tag_slug):
+#    tag = get_object_or_404(TagPost, slug=tag_slug)
+#    posts = tag.tags.filter(is_published=MySite.Status.PUBLISHED)
+#    data = {
+#        'title': f'Тег: {tag.tags}',
+#        'menu': menu,
+#        'posts': posts,
+#        'cat_selected': None,
+#    }
+#    return render(request, 'mysite/index.html', context=data)
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(MySite, slug=post_slug)
-    data = {'title': post.title,
-            'menu': menu,
-            'post': post,
-            'cat_selected': 0,
-            }
-    return render(request, 'mysite/post.html', context=data)
+class MySiteCategory(DataMixin, ListView):
+    template_name = 'mysite/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cat = context['posts'][0].cat
+        return self.get_mixin_context(context,
+                                      title='Категория - ' + cat.name,
+                                      cat_selected=cat.id, )
+
+    def get_queryset(self):
+        return (MySite.published.filter(cat__slug=self.kwargs['cat_slug'])
+                .select_related('cat'))
 
 
-def index(request):  # HttpRequest
-    posts = MySite.objects.filter(is_published=1)
-    data = {
-        'title': 'Главная страница',
-        'menu': menu,
-        'posts': MySite.published.all(),
-        'cat_selected': 0,
-    }
-    return render(request, 'mysite/index.html', context=data)
+#def show_category(request, cat_slug):
+#    category = get_object_or_404(Category, slug=cat_slug)
+#    posts = MySite.published.filter(cat_id=category.pk)
+#    data = {
+#        'title': f'Рубрика: {category.name}',
+#        'menu': menu,
+#        'posts': posts,
+#        'cat_selected': category.pk,
+#    }
+#    return render(request, 'mysite/index.html', context=data)
+
+
+class ShowPost(DataMixin, DetailView):
+    model = MySite
+    template_name = 'mysite/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return self.get_mixin_context(context, title=context['post'])
+
+    def get_object(self, queryset=None):
+        return (get_object_or_404
+                (MySite.published,
+                 slug=self.kwargs[self.slug_url_kwarg]))
+
+
+#def show_post(request, post_slug):
+#    post = get_object_or_404(MySite, slug=post_slug)
+#    data = {'title': post.title,
+#            'menu': menu,
+#            'post': post,
+#            'cat_selected': 0,
+#            }
+#    return render(request, 'mysite/post.html', context=data)
+
+
+class MySiteHome(DataMixin, ListView):
+    template_name = 'mysite/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        return self.get_mixin_context(super().get_context_data(**kwargs),
+                                      title='Главная страница',
+                                      cat_selected=0, )
+
+    def get_queryset(self):
+        return MySite.published.all().select_related('cat')
+
+#def index(request):  # HttpRequest
+#    posts = MySite.objects.filter(is_published=1)
+#    data = {
+#        'title': 'Главная страница',
+#        'menu': menu,
+#        'posts': MySite.published.all(),
+#        'cat_selected': 0,
+#    }
+#    return render(request, 'mysite/index.html', context=data)
 
 
 def handle_uploaded_file(f):
@@ -72,6 +139,10 @@ def handle_uploaded_file(f):
 
 
 def about(request):
+    contact_list = MySite.published.all()
+    paginator = Paginator(contact_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -80,22 +151,54 @@ def about(request):
     else:
         form = UploadFileForm()
     return render(request, 'mysite/about.html',
-                  {'title': 'О сайте',
-                   'menu': menu, 'form': form})
+                  {'page_obj': page_obj, 'title': 'О сайте'})
 
 
-def addpage(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'mysite/addpage.html',
-                  {'menu': menu,
-                   'title': 'Добавление статьи',
-                   'form': form})
+#def addpage(request):
+#    if request.method == 'POST':
+#        form = AddPostForm(request.POST, request.FILES)
+#        if form.is_valid():
+#            form.save()
+#            return redirect('home')
+#    else:
+#        form = AddPostForm()
+#    return render(request, 'mysite/addpage.html',
+#                  {'menu': menu,
+#                   'title': 'Добавление статьи',
+#                   'form': form})
+
+
+class AddPage(DataMixin, CreateView):
+    model = MySite
+    #form_class = AddPostForm
+    fields = ['title', 'slug', 'content', 'is_published', 'cat', 'isbn', 'tags']
+    template_name = 'mysite/addpage.html'
+    success_url = reverse_lazy('home')
+    title_page = 'Добавление статьи'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class UpdatePage(DataMixin, UpdateView):
+    model = MySite
+    fields = ['title', 'slug', 'content', 'is_published', 'cat', 'isbn', 'tags']
+    template_name = 'mysite/addpage.html'
+    success_url = reverse_lazy('home')
+    title_page = 'Редактирование статьи'
+
+
+class DeletePage(DataMixin, DeleteView):
+    model = MySite
+    template_name = 'mysite/deletepage.html'
+    success_url = reverse_lazy('home')
+    title_page = 'Удаление статьи'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(self.extra_context)
+        return context
 
 
 def contact(request):
